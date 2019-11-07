c2d2eab50c932aa9a053ddf8b3d83593b71aea04
Gabriel B. Sant'Anna <baiocchi.gabriel@gmail.com>
2019-11-07 00:22:22 -0300

    Try stochastic simulation
    Scripts folder is now partitioned

--- src/stochastic/stochastic.m
+++ src/stochastic/stochastic.m
@@ -1,7 +1,8 @@
 % ============================== OCTAVE SETUP ==================================
     clear;
     format long;
-    pkg load statistics;
+
+
 % ========================== MODEL REACTION RATES ==============================
 
     % translation rates (1/s)
@@ -71,10 +72,8 @@
     Is = (6 .- 25*cos(simulation .* 2*pi/0.9e5) .+ 25) * 1e-9; % sinusoidal
 
     % initial state concentrations (M)
-    R1 = 50e-9;
-    R2 = 50e-9;
-    R3 = 0e-9;
-    R4 = 0e-9;
+    R1 = R2 = 50e-9;
+    R3 = R4 = 0e-9;
 
 
 % =============================== SIMULATION ===================================
@@ -96,6 +95,8 @@
     Kr_R1P5 ^= Nr_R1P5;
     Kr_R1P6 ^= Nr_R1P6;
     Kr_R2P6 ^= Nr_R2P6;
+    V = 1e9;
+    isqrV = 1 / sqrt(V);
 
     % abstractions for activation & repression Hill-function
     % these consider K^n is being passed in already precomputed
@@ -107,6 +108,11 @@
         y = 1 / (1 + (X^n / Kn));
     endfunction
 
+    % generate Gaussian noise with zero mean and variance one
+    random_seed = 73544911520192;
+    randn('seed', random_seed);
+    noise = randn(length(simulation), 1);
+
     % actual simulation
     for t = 1 : length(simulation)
 
@@ -117,38 +123,28 @@
         R4s(t) = R4;
 
         % common subexpression optimization (used below)
-        AP1 = Ha(Is(t), Na_P1, Ka_P1);
-        AP2 = Ha(Is(t), Na_P2, Ka_P2);
-        RP1 = Hr(R2, Nr_R2P1, Kr_R2P1);
-        RP2 = Hr(R4, Nr_R4P2, Kr_R4P2);
-
-        % simplified coefficients
-        a1 = (Kt_R1 * Kb_P1 / Kd_mR1P1) * AP1 * RP1;
+        activationP1 = Ha(Is(t), Na_P1, Ka_P1);
+        repressionP1 = Hr(R2, Nr_R2P1, Kr_R2P1);
+        activationP2 = Ha(Is(t), Na_P2, Ka_P2);
+        repressionP2 = Hr(R4, Nr_R4P2, Kr_R4P2);
+        a1 = (Kt_R1 * Kb_P1 / Kd_mR1P1) * activationP1 * repressionP1;
         a2 = (Kt_R1 * Kb_P3 / Kd_mR1P3) * Hr(R3, Nr_R3P3, Kr_R3P3);
         a3 = Kd_R1 * R1;
-        b1 = (Kt_R2 * Kb_P2 / Kd_mR2P2) * AP2 * RP2;
+        b1 = (Kt_R2 * Kb_P2 / Kd_mR2P2) * activationP2 * repressionP2;
         b2 = (Kt_R2 * Kb_P4 / Kd_mR2P4) * Hr(R3, Nr_R3P4, Kr_R3P4) * Hr(R4, Nr_R4P4, Kr_R4P4);
         b3 = Kd_R2 * R2;
-        c1 = (Kt_R3 * Kb_P2 / Kd_mR3P2) * AP2 * RP2;
+        c1 = (Kt_R3 * Kb_P2 / Kd_mR3P2) * activationP2 * repressionP2;
         c2 = (Kt_R3 * Kb_P5 / Kd_mR3P5) * Hr(R1, Nr_R1P5, Kr_R1P5);
         c3 = Kd_R3 * R3;
-        d1 = (Kt_R4 * Kb_P1 / Kd_mR4P1) * AP1 * RP1;
+        d1 = (Kt_R4 * Kb_P1 / Kd_mR4P1) * activationP1 * repressionP1;
         d2 = (Kt_R4 * Kb_P6 / Kd_mR4P6) * Hr(R1, Nr_R1P6, Kr_R1P6) * Hr(R2, Nr_R2P6, Kr_R2P6);
         d3 = Kd_R4*R4;
 
-        V = 1e9;
-
-        noise = stdnormal_rnd(1)
-
         % compute variation
-        dR1dt = a1 + a2 - a3 + ((1/sqrt(V)) * ((sqrt(a1) * noise) + (sqrt(a2) *
-        noise) - (sqrt(a3) * noise)));
-        dR2dt = b1 + b2 - b3 + ((1/sqrt(V)) * ((sqrt(b1) * noise) + (sqrt(b2) *
-        noise) - (sqrt(b3) * noise)));
-        dR3dt = c1 + c2 - c3 + ((1/sqrt(V)) * ((sqrt(c1) * noise) + (sqrt(c2) *
-        noise) - (sqrt(c3) * noise)));
-        dR4dt = d1 + d2 - d3 + ((1/sqrt(V)) * ((sqrt(d1) * noise) + (sqrt(d2) *
-        noise) - (sqrt(d3) * noise)));
+        dR1dt = a1 + a2 - a3 + isqrV*(sqrt(a1)*noise(t) + sqrt(a2)*noise(t) - sqrt(a3)*noise(t));
+        dR2dt = b1 + b2 - b3 + isqrV*(sqrt(b1)*noise(t) + sqrt(b2)*noise(t) - sqrt(b3)*noise(t));
+        dR3dt = c1 + c2 - c3 + isqrV*(sqrt(c1)*noise(t) + sqrt(c2)*noise(t) - sqrt(c3)*noise(t));
+        dR4dt = d1 + d2 - d3 + isqrV*(sqrt(d1)*noise(t) + sqrt(d2)*noise(t) - sqrt(d3)*noise(t));
 
         % apply state changes
         R1 += dR1dt * dt;
@@ -167,23 +163,23 @@
     % scale data for easier visualization
     timescale = 1e5;
     quantscale = 1e-9;
-    simulation /= timescale;
-    Is  /= quantscale;
-    R1s /= quantscale;
-    R2s /= quantscale;
-    R3s /= quantscale;
-    R4s /= quantscale;
+    x = simulation / timescale;
+    yI = Is / quantscale;
+    yR1 = R1s / quantscale;
+    yR2 = R2s / quantscale;
+    yR3 = R3s / quantscale;
+    yR4 = R4s / quantscale;
 
-    % model suplot
+    % model subplot
     subplot(2, 1, 1);
-    plot(simulation,R1s,'-m;R1;', simulation,R2s,'-k;R2;', simulation,R3s,'-r;R3;', simulation,R4s,'-g;R4;');
+    plot(x,yR1,'-m;R1;', x,yR2,'-k;R2;', x,yR3,'-r;R3;', x,yR4,'-g;R4;');
     xlabel("Time (10^5 seconds)");
     ylabel("Concentration (nM)");
-    title("Reduced Model");
+    title("Reduced Model Stochastics");
 
     % input subplot
     subplot(2, 1, 2);
-    plot(simulation, Is, 'b;I;');
+    plot(x, yI, 'b;I;');
     xlabel("Time (10^5 seconds)");
     ylabel("Concentration (nM)");
 
