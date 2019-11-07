2999fc7a8a8f5ac4880bf666ca21a5eb3c764835
Gabriel B. Sant'Anna <baiocchi.gabriel@gmail.com>
2019-11-07 09:45:40 -0300

    Refactor switch code

--- src/switch/switch_full.m
+++ src/switch/switch_full.m
@@ -69,76 +69,66 @@
     Kr_R1P6 = 6e-10;
     Kr_R2P6 = 6e-10;
 
-    Kr_R2P1_2 = 4e-6; % Switch unique variables
-    Kr_R4P2_2 = 4e-6; % Switch unique variables
-    Kr_R3P3_2 = 4e-6; % Switch unique variables
-    Kr_R3P4_2 = 4e-6; % Switch unique variables
-    Kr_R4P4_2 = 4e-6; % Switch unique variables
-    Kr_R1P5_2 = 4e-6; % Switch unique variables
-    Kr_R1P6_2 = 4e-6; % Switch unique variables
-    Kr_R2P6_2 = 4e-6; % Switch unique variables
-
 
 % ============================ INPUT PARAMETERS ================================
 
     % time settings
     dt = 60;                     % 1 minute timesteps
-    simulation = 0 : dt : 3.5e5; % simulation length control    
+    simulation = 0 : dt : 3.5e5; % simulation length control
+
+    % input vector
+    Is = zeros(1, length(simulation));
 
     % reaction initial conditions for each run
-    SI =   1e-9 * [0.1, 0.1, 50 , 50]; % constant input
-    SR12 = 1e-9 * [50 , 0  , 50, 0];
-    SR34 = 1e-9 * [0  , 50 , 0 , 50];
+    multiI   = 1e-9 * [0.1, 0.1, 50, 50]; % constant input
+    multiR12 = 1e-9 * [50 , 0  , 50, 0 ];
+    multiR34 = 1e-9 * [0  , 50 , 0 , 50];
 
-    % initial state concentrations (M)
-    mR1P1 = 0e-9;
-    mR4P1 = 0e-9;
-    mR2P2 = 0e-9;
-    mR3P2 = 0e-9;
-    mR1P3 = 0e-9;
-    mR2P4 = 0e-9;
-    mR3P5 = 0e-9;
-    mR4P6 = 0e-9;
+    % trigger hold & release times
+    t_hold = 1.5e5 / dt;
+    t_release = 1.55e5 / dt;
+
+    % induced increase in repressor binding affinity used to trigger the switch
+    switch_trigger = 6e-10 - 4e-6;
+
+    % save original Hill repression constants
+    Kr_R2P1_ori = Kr_R2P1;
+    Kr_R4P2_ori = Kr_R4P2;
+    Kr_R3P3_ori = Kr_R3P3;
+    Kr_R3P4_ori = Kr_R3P4;
+    Kr_R4P4_ori = Kr_R4P4;
+    Kr_R1P5_ori = Kr_R1P5;
+    Kr_R1P6_ori = Kr_R1P6;
+    Kr_R2P6_ori = Kr_R2P6;
 
 
 % =============================== SIMULATION ===================================
 
-    % preallocate arrays to store concentrations over time
-    R1s = zeros(1, length(simulation));
-    R2s = zeros(1, length(simulation));
-    R3s = zeros(1, length(simulation));
-    R4s = zeros(1, length(simulation));
-    mR1P1s = zeros(1, length(simulation));
-    mR4P1s = zeros(1, length(simulation));
-    mR2P2s = zeros(1, length(simulation));
-    mR3P2s = zeros(1, length(simulation));
-    mR1P3s = zeros(1, length(simulation));
-    mR2P4s = zeros(1, length(simulation));
-    mR3P5s = zeros(1, length(simulation));
-    mR4P6s = zeros(1, length(simulation));
+    % for each set of parameters, make a simulation run and display its results
+    for sim = 1 : length(multiI)
 
-    % precompute loop invariants
-    Ka_P1 ^= Na_P1;
-    Ka_P2 ^= Na_P2;
-    Kr_R2P1 ^= Nr_R2P1;
-    Kr_R4P2 ^= Nr_R4P2;
-    Kr_R3P3 ^= Nr_R3P3;
-    Kr_R3P4 ^= Nr_R3P4;
-    Kr_R4P4 ^= Nr_R4P4;
-    Kr_R1P5 ^= Nr_R1P5;
-    Kr_R1P6 ^= Nr_R1P6;
-    Kr_R2P6 ^= Nr_R2P6;
+        % set initial state concentrations
+        Is(:) = multiI(sim);
+        R1 = multiR12(sim);
+        R2 = multiR12(sim);
+        R3 = multiR34(sim);
+        R4 = multiR34(sim);
+        mR1P1 = 0e-9;
+        mR4P1 = 0e-9;
+        mR2P2 = 0e-9;
+        mR3P2 = 0e-9;
+        mR1P3 = 0e-9;
+        mR2P4 = 0e-9;
+        mR3P5 = 0e-9;
+        mR4P6 = 0e-9;
 
-    % actual simulation
-    for s = 1 : length(SI)
+        % preallocate arrays to store concentrations over time
+        R1s = zeros(1, length(simulation));
+        R2s = zeros(1, length(simulation));
+        R3s = zeros(1, length(simulation));
+        R4s = zeros(1, length(simulation));
 
-        Is = zeros(1, length(simulation));
-        Is(:) = SI(s);
-        R1 = SR12(s);
-        R2 = SR12(s);
-        R3 = SR34(s);
-        R4 = SR34(s);
-
+        % actual simulation
         for t = 1 : length(simulation)
 
             % store current state
@@ -146,78 +136,52 @@
             R2s(t) = R2;
             R3s(t) = R3;
             R4s(t) = R4;
-            mR1P1s(t) = mR1P1;
-            mR4P1s(t) = mR4P1;
-            mR2P2s(t) = mR2P2;
-            mR3P2s(t) = mR3P2;
-            mR1P3s(t) = mR1P3;
-            mR2P4s(t) = mR2P4;
-            mR3P5s(t) = mR3P5;
-            mR4P6s(t) = mR4P6;
+
+            % change binding following hold or release of switch trigger
+            if t >= t_hold && t <= t_release
+                if sim == 1 || sim == 3
+                    Kr_R1P5 = Kr_R1P5_ori + switch_trigger;
+                    Kr_R1P6 = Kr_R1P6_ori + switch_trigger;
+                    Kr_R2P1 = Kr_R2P1_ori + switch_trigger;
+                    Kr_R2P6 = Kr_R2P6_ori + switch_trigger;
+                elseif sim == 2 || sim == 4
+                    Kr_R3P3 = Kr_R3P3_ori + switch_trigger;
+                    Kr_R3P4 = Kr_R3P4_ori + switch_trigger;
+                    Kr_R4P2 = Kr_R4P2_ori + switch_trigger;
+                    Kr_R4P4 = Kr_R4P4_ori + switch_trigger;
+                endif
+            else
+                Kr_R2P1 = Kr_R2P1_ori;
+                Kr_R4P2 = Kr_R4P2_ori;
+                Kr_R3P3 = Kr_R3P3_ori;
+                Kr_R3P4 = Kr_R3P4_ori;
+                Kr_R4P4 = Kr_R4P4_ori;
+                Kr_R1P5 = Kr_R1P5_ori;
+                Kr_R1P6 = Kr_R1P6_ori;
+                Kr_R2P6 = Kr_R2P6_ori;
+            endif
 
             % common subexpression optimization (used below)
             IsP1 = Is(t)^Na_P1;
+            activationP1 = IsP1 / (Ka_P1^Na_P1 + IsP1);
+            repressionP1 = 1 / (1 + (R2/Kr_R2P1)^Nr_R2P1);
             IsP2 = Is(t)^Na_P2;
+            activationP2 = IsP2 / (Ka_P2^Na_P2 + IsP2);
+            repressionP2 = 1 / (1 + (R4/Kr_R4P2)^Nr_R4P2);
 
             % compute variation
-            dR1dt = Kt_R1 * (mR1P1 + mR1P3) - Kd_R1 * R1;
-            dR2dt = Kt_R2 * (mR2P2 + mR2P4) - Kd_R2 * R2;
-            dR3dt = Kt_R3 * (mR3P2 + mR3P5) - Kd_R3 * R3;
-            dR4dt = Kt_R4 * (mR4P1 + mR4P6) - Kd_R4 * R4;
-
-            if s == 1 || s == 3
-
-                if t >= 2500 && t <= 2584
-
-                    dmR1P1dt = Kc_P1 + Kb_P1 * (IsP1 / (Ka_P1 + IsP1)) * (1 / (1 + (R2^Nr_R2P1 / Kr_R2P1_2))) - Kd_mR1P1 * mR1P1;
-                    dmR4P1dt = Kc_P1 + Kb_P1 * (IsP1 / (Ka_P1 + IsP1)) * (1 / (1 + (R2^Nr_R2P1 / Kr_R2P1_2))) - Kd_mR4P1 * mR4P1;
-                    dmR2P2dt = Kc_P2 + Kb_P2 * (IsP2 / (Ka_P2 + IsP2)) * (1 / (1 + (R4^Nr_R4P2 / Kr_R4P2))) - Kd_mR2P2 * mR2P2;
-                    dmR3P2dt = Kc_P2 + Kb_P2 * (IsP2 / (Ka_P2 + IsP2)) * (1 / (1 + (R4^Nr_R4P2 / Kr_R4P2))) - Kd_mR3P2 * mR3P2;
-                    dmR1P3dt = Kc_P3 + Kb_P3 * (1 / (1 + (R3^Nr_R3P3 / Kr_R3P3))) - Kd_mR1P3 * mR1P3;
-                    dmR2P4dt = Kc_P4 + Kb_P4 * (1 / (1 + (R3^Nr_R3P4 / Kr_R3P4))) * (1 / (1 + (R4^Nr_R4P4 / Kr_R4P4))) - Kd_mR2P4 * mR2P4;
-                    dmR3P5dt = Kc_P5 + Kb_P5 * (1 / (1 + (R1^Nr_R1P5 / Kr_R1P5_2))) - Kd_mR3P5 * mR3P5;
-                    dmR4P6dt = Kc_P6 + Kb_P6 * (1 / (1 + (R1^Nr_R1P6 / Kr_R1P6_2))) * (1 / (1 + (R2^Nr_R2P6 / Kr_R2P6_2))) - Kd_mR4P6 * mR4P6;
-
-                else
-
-                    dmR1P1dt = Kc_P1 + Kb_P1 * (IsP1 / (Ka_P1 + IsP1)) * (1 / (1 + (R2^Nr_R2P1 / Kr_R2P1))) - Kd_mR1P1 * mR1P1;
-                    dmR4P1dt = Kc_P1 + Kb_P1 * (IsP1 / (Ka_P1 + IsP1)) * (1 / (1 + (R2^Nr_R2P1 / Kr_R2P1))) - Kd_mR4P1 * mR4P1;
-                    dmR2P2dt = Kc_P2 + Kb_P2 * (IsP2 / (Ka_P2 + IsP2)) * (1 / (1 + (R4^Nr_R4P2 / Kr_R4P2))) - Kd_mR2P2 * mR2P2;
-                    dmR3P2dt = Kc_P2 + Kb_P2 * (IsP2 / (Ka_P2 + IsP2)) * (1 / (1 + (R4^Nr_R4P2 / Kr_R4P2))) - Kd_mR3P2 * mR3P2;
-                    dmR1P3dt = Kc_P3 + Kb_P3 * (1 / (1 + (R3^Nr_R3P3 / Kr_R3P3))) - Kd_mR1P3 * mR1P3;
-                    dmR2P4dt = Kc_P4 + Kb_P4 * (1 / (1 + (R3^Nr_R3P4 / Kr_R3P4))) * (1 / (1 + (R4^Nr_R4P4 / Kr_R4P4))) - Kd_mR2P4 * mR2P4;
-                    dmR3P5dt = Kc_P5 + Kb_P5 * (1 / (1 + (R1^Nr_R1P5 / Kr_R1P5))) - Kd_mR3P5 * mR3P5;
-                    dmR4P6dt = Kc_P6 + Kb_P6 * (1 / (1 + (R1^Nr_R1P6 / Kr_R1P6))) * (1 / (1 + (R2^Nr_R2P6 / Kr_R2P6))) - Kd_mR4P6 * mR4P6;
-
-                endif
-
-            else
-
-                if t >= 2500 && t <= 2584
-
-                    dmR1P1dt = Kc_P1 + Kb_P1 * (IsP1 / (Ka_P1 + IsP1)) * (1 / (1 + (R2^Nr_R2P1 / Kr_R2P1))) - Kd_mR1P1 * mR1P1;
-                    dmR4P1dt = Kc_P1 + Kb_P1 * (IsP1 / (Ka_P1 + IsP1)) * (1 / (1 + (R2^Nr_R2P1 / Kr_R2P1))) - Kd_mR4P1 * mR4P1;
-                    dmR2P2dt = Kc_P2 + Kb_P2 * (IsP2 / (Ka_P2 + IsP2)) * (1 / (1 + (R4^Nr_R4P2 / Kr_R4P2_2))) - Kd_mR2P2 * mR2P2;
-                    dmR3P2dt = Kc_P2 + Kb_P2 * (IsP2 / (Ka_P2 + IsP2)) * (1 / (1 + (R4^Nr_R4P2 / Kr_R4P2_2))) - Kd_mR3P2 * mR3P2;
-                    dmR1P3dt = Kc_P3 + Kb_P3 * (1 / (1 + (R3^Nr_R3P3 / Kr_R3P3_2))) - Kd_mR1P3 * mR1P3;
-                    dmR2P4dt = Kc_P4 + Kb_P4 * (1 / (1 + (R3^Nr_R3P4 / Kr_R3P4_2))) * (1 / (1 + (R4^Nr_R4P4 / Kr_R4P4_2))) - Kd_mR2P4 * mR2P4;
-                    dmR3P5dt = Kc_P5 + Kb_P5 * (1 / (1 + (R1^Nr_R1P5 / Kr_R1P5))) - Kd_mR3P5 * mR3P5;
-                    dmR4P6dt = Kc_P6 + Kb_P6 * (1 / (1 + (R1^Nr_R1P6 / Kr_R1P6))) * (1 / (1 + (R2^Nr_R2P6 / Kr_R2P6))) - Kd_mR4P6 * mR4P6;
-
-                else
-
-                    dmR1P1dt = Kc_P1 + Kb_P1 * (IsP1 / (Ka_P1 + IsP1)) * (1 / (1 + (R2^Nr_R2P1 / Kr_R2P1))) - Kd_mR1P1 * mR1P1;
-                    dmR4P1dt = Kc_P1 + Kb_P1 * (IsP1 / (Ka_P1 + IsP1)) * (1 / (1 + (R2^Nr_R2P1 / Kr_R2P1))) - Kd_mR4P1 * mR4P1;
-                    dmR2P2dt = Kc_P2 + Kb_P2 * (IsP2 / (Ka_P2 + IsP2)) * (1 / (1 + (R4^Nr_R4P2 / Kr_R4P2))) - Kd_mR2P2 * mR2P2;
-                    dmR3P2dt = Kc_P2 + Kb_P2 * (IsP2 / (Ka_P2 + IsP2)) * (1 / (1 + (R4^Nr_R4P2 / Kr_R4P2))) - Kd_mR3P2 * mR3P2;
-                    dmR1P3dt = Kc_P3 + Kb_P3 * (1 / (1 + (R3^Nr_R3P3 / Kr_R3P3))) - Kd_mR1P3 * mR1P3;
-                    dmR2P4dt = Kc_P4 + Kb_P4 * (1 / (1 + (R3^Nr_R3P4 / Kr_R3P4))) * (1 / (1 + (R4^Nr_R4P4 / Kr_R4P4))) - Kd_mR2P4 * mR2P4;
-                    dmR3P5dt = Kc_P5 + Kb_P5 * (1 / (1 + (R1^Nr_R1P5 / Kr_R1P5))) - Kd_mR3P5 * mR3P5;
-                    dmR4P6dt = Kc_P6 + Kb_P6 * (1 / (1 + (R1^Nr_R1P6 / Kr_R1P6))) * (1 / (1 + (R2^Nr_R2P6 / Kr_R2P6))) - Kd_mR4P6 * mR4P6;
-
-                endif
-
-            endif
+            dR1dt = Kt_R1*(mR1P1 + mR1P3) - Kd_R1*R1;
+            dR2dt = Kt_R2*(mR2P2 + mR2P4) - Kd_R2*R2;
+            dR3dt = Kt_R3*(mR3P2 + mR3P5) - Kd_R3*R3;
+            dR4dt = Kt_R4*(mR4P1 + mR4P6) - Kd_R4*R4;
+            dmR1P1dt = Kc_P1 + Kb_P1*activationP1*repressionP1 - Kd_mR1P1*mR1P1;
+            dmR4P1dt = Kc_P1 + Kb_P1*activationP1*repressionP1 - Kd_mR4P1*mR4P1;
+            dmR2P2dt = Kc_P2 + Kb_P2*activationP2*repressionP2 - Kd_mR2P2*mR2P2;
+            dmR3P2dt = Kc_P2 + Kb_P2*activationP2*repressionP2 - Kd_mR3P2*mR3P2;
+            dmR1P3dt = Kc_P3 + Kb_P3 * (1 / (1 + (R3/Kr_R3P3)^Nr_R3P3)) - Kd_mR1P3*mR1P3;
+            dmR2P4dt = Kc_P4 + Kb_P4 * (1 / (1 + (R3/Kr_R3P4)^Nr_R3P4)) * (1 / (1 + (R4/Kr_R4P4)^Nr_R4P4)) - Kd_mR2P4*mR2P4;
+            dmR3P5dt = Kc_P5 + Kb_P5 * (1 / (1 + (R1/Kr_R1P5)^Nr_R1P5)) - Kd_mR3P5*mR3P5;
+            dmR4P6dt = Kc_P6 + Kb_P6 * (1 / (1 + (R1/Kr_R1P6)^Nr_R1P6)) * (1 / (1 + (R2/Kr_R2P6)^Nr_R2P6)) - Kd_mR4P6*mR4P6;
 
             % apply state changes
             R1 += dR1dt * dt;
@@ -235,30 +199,25 @@
 
         endfor
 
-        % ======================== RESULTS ============================
-
-        figure;
+        figure; % plot each graphic on a separate window
         hold on;
 
         % scale data for easier visualization
         timescale = 1e5;
         quantscale = 1e-9;
-        simulation /= timescale;
-        Is  /= quantscale;
-        R1s /= quantscale;
-        R2s /= quantscale;
-        R3s /= quantscale;
-        R4s /= quantscale;
+        x = simulation / timescale;
+        yR1 = R1s / quantscale;
+        yR2 = R2s / quantscale;
+        yR3 = R3s / quantscale;
+        yR4 = R4s / quantscale;
 
-        % model suplot
-        subplot(2, 1, 1);
-        plot(simulation,R1s,'-m;R1;', simulation,R2s,'-k;R2;', simulation,R3s,'-r;R3;', simulation,R4s,'-g;R4;');
+        % plot results
+        plot(x,yR1,'-m;R1;', x,yR2,'-k;R2;', x,yR3,'-r;R3;', x,yR4,'-g;R4;');
         xlabel("Time (10^5 seconds)");
         ylabel("Concentration (nM)");
-        title("Full Model");
+        title("Switch Function (Full Model)");
 
         hold off;
 
     endfor
-
     a = input("\nPress enter to exit ");
